97ec09176c7b66e0fa6354b64f7371902d68a814
Rena Li <rli553@uwo.ca>
2023-06-21 23:25:02 -0400

    First Commit

---
+++ amazon-clone/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import React from "react";
+import Header from "./Header";
+
+function App() {
+  return (
+    // BEM
+    <div className="App">
+      <Header />
+      {/* Home */}
+    </div>
+  );
+}
+
+export default App;
---
+++ amazon-clone/src/Firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDcjJa7-WT2a4_TO17kunhT50GiQ2NSEKQ",
+    authDomain: "clone-e3ed3.firebaseapp.com",
+    projectId: "clone-e3ed3",
+    storageBucket: "clone-e3ed3.appspot.com",
+    messagingSenderId: "75183775126",
+    appId: "1:75183775126:web:791f8f610df0a6c2c111b7",
+    measurementId: "G-SDJK1XE5C9"
+  };
