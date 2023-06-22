d2c121ed2eec0592497ba5481370b3b71d947408
Rena Li <rli553@uwo.ca>
2023-06-22 17:48:43 -0400

    Cleaning up

--- amazon-clone/src/Header.js
+++ amazon-clone/src/Header.js
@@ -1,43 +1,54 @@
 import React from 'react';
 import './Header.css';
 import SearchIcon from '@mui/icons-material/Search';
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
 
 function Header() {
   return (
     <div className='header'>
+
       <img
         className="header__logo"
         src='https://pngimg.com/uploads/amazon/amazon_PNG11.png'
       />
 
-      <div
-      className="header__search">
+      <div className="header__option">
+        <span className='header__optionLineOne'>Hello</span>
+          <span className='header__optionLineTwo'>Select your address</span>
+      </div>
+
+      <div className="header__search">
         <input className="header__searchInput" type="text" />
         <SearchIcon className="header__searchIcon" />
       </div>
 
-      <div className="header__nav"></div>
+      <div className="header__nav">
+
         <div className="header__option">
-          <span className='header__optionLineOne'>Hello Guest</span>
-          <span className='header__optionLineTwo'>Sign In</span>
+          <span className='header__optionLineOther'>Filler</span>
+          <span className='header__optionLineTwo'>EN</span>
         </div>
 
         <div className="header__option">
-        <span className='header__optionLineOne'>Hello Guest</span>
-          <span className='header__optionLineTwo'>Sign In</span>
+          <span className='header__optionLineOne'>Hello, sign in</span>
+          <span className='header__optionLineTwo'>Accounts & Lists</span>
         </div>
 
         <div className="header__option">
-        <span className='header__optionLineOne'>Returns</span>
+          <span className='header__optionLineOne'>Returns</span>
           <span className='header__optionLineTwo'>& Orders</span>
         </div>
 
-        <div className="header__option">
-        <span className='header__optionLineOne'>Your</span>
-          <span className='header__optionLineTwo'>Prime</span>
+        <div className="header__optionBasket">
+          <AddShoppingCartIcon />
+          <span className="header__optionLineTwo header__basketCount">0</span>
         </div>
+
     </div>
-  )
+  </div>
+
+
+  );
 }
 
 export default Header;
